907bb4bc4afffe49bbf2c9aa400d2472cd52fc3e
Benjamin Dionysus <benjamin@sixbynine.com>
2020-10-26 13:50:02 -0500

    mock data entered

---
+++ pseudocode.js
@@ -0,0 +1,46 @@
+// todo #1 Admin database
+// todo id
+// todo name
+// todo password
+// todo access level
+
+// todo #2 Project Categories
+// todo id
+// todo title
+// todo description
+// todo tab name
+// todo thumbnail
+// todo added by
+// todo display order
+
+// todo #3 projects
+// todo id
+// todo projectId
+// todo description
+// todo github link
+// todo thumbnail
+// todo image gallery link
+// todo display order
+// todo show in portfolio?
+
+// todo #4 main page
+// todo #5 main display area with tabs for every project
+// todo #6 main display shows thumbnails for every project category
+
+// todo #7 Tab page
+// todo #8 Tab display area with tab for every project plu smain
+// todo #9 tab display shows thumbnails for ever 
+
+// todo #10 admin page
+// todo #11 login page
+// todo #12 new project category, with tabs for edit project categories
+// todo #13 edit project category, with tabs for edit project
+// todo #14 edit project with tabs for edit project categories
+
+// todo #15 make fake data
+// todo #16 write index shell
+// todo #17 write index.js for main page + tabs
+// todo #18 write get routes
+// todo #19 write server.js
+
+// todo #20 write index.js for project page + tabs
