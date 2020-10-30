4fb65b69cd02600303aef353b1a89f2cdf2deb6b
Benjamin Dionysus <benjamin@sixbynine.com>
2020-10-30 10:00:15 -0500

    Jaws DB

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
+if(process.env.JAWSDB_URL){
+  connection = mysql.connection(process.env.JAWSDB_URL);
+}
+else 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
